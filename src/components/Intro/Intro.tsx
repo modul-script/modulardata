8530c434040fd4b645286efba0d561ccaf1b02f9
Miodrag <wmiodrag@gmail.com>
2021-09-06 18:34:16 +0200

    Template

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -5,6 +5,5 @@ export const Intro: FunctionComponent = () => (
   <S.Container>
     <S.StyledLogo />
     <S.Paragraph>Delete this file to start your project.</S.Paragraph>
-    <S.Link href="https://github.com/msallent/gatsby-starter-skeleton">Gatsby Skeleton</S.Link>
   </S.Container>
 );
